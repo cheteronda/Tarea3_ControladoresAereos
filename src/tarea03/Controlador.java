720fd68ed281c3228b55e321b9af911e9f12a88c
Alfonso <Alfonso@Chete.mshome.net>
2025-01-08 01:24:28 +0100

    Inicio del programa de la clase controlador

---
+++ src/tarea03/Controlador.java
@@ -0,0 +1,142 @@
+
+package tarea03;
+import java.lang.Object;
+// ------------------------------------------------------------
+//                   Clase Controlador
+// ------------------------------------------------------------
+/**
+ * <p>
+ * Clase que representa al <strong>controlador</strong>,
+ * que será la clase que utilizaremos y donde se escribirán las diferentes 
+ * operaciones en aeronaves y aeropuertos que se nos pide en 
+ * el enunciado de la tarea.</p>
+ *
+ * @author Morales Garcia Alfonso
+ */
+public class Controlador {
+    public static void main(String[] args) {
+        
+        //----------------------------------------------
+        //          Declaración de variables 
+        //----------------------------------------------
+        
+       
+        
+        //----------------------------------------------
+        //          Creación de objetos
+        //----------------------------------------------
+        
+       
+        
+        // Instanciar los 3 aeropuertos: Barcelona, Madrid, Granada.
+        
+        
+        
+        // Instanciar las 3 aeronaves: Avion1, Avion2, Avion3.
+       
+        
+        
+        //----------------------------------------------
+        //   Inicio de la secuencia de instrucciones
+        //----------------------------------------------
+        
+       
+        
+        //Avion1 despega con velocidad 1500, altitud 1750, rumbo 50 y fechaHora actual
+        
+        
+        //Avion2 despega con velocidad 1500, altitud 1850 y rumbo 75
+       
+        
+        //Avion3 despega con velocidad 1500, altitud 1000 y rumbo 180
+        
+        
+        
+        
+        //Comprobar si Avion1 está volando
+       
+        
+        //Mostrar la matrícula del Avion2
+        
+        
+        //Mostrar modelo del Avion3
+        
+        
+        //Modificar rumbo del Avion2 a 90º y mostrarlo
+        
+        
+        
+        
+        //Avion3 aterriza en el aeropuerto de Barcelona despues de 75 minutos
+        
+        
+        //Avion2 aterriza en el aeropuerto de Madrid despues de 80 minutos
+        
+        
+        
+        
+        //Comprobar si Avion2 está volando
+        
+        
+        //Modificar altitud del Avion1 a 1250 metros y mostrarlo
+        
+        
+        
+        //Mostrar el tiempo toal de vuelo del Avion2
+        
+        
+        //Mostrar el aeropuerto del Avion3
+        
+        
+        //Mostrar la fecha y hora de despegue del Avion1
+        
+        
+        
+        //Avion3 despega con velocidad 860, altitud 1420 y rumbo 270
+        
+        
+        //Avion1 aterriza en el aeropuerto de Granada despues de 260 minutos
+        
+        
+        
+        
+        //Modificar velocidad del Avion3 a 950km/h y mostrarlo
+        
+        
+        
+        //Mostrar el nombre del aeropuerto de Madrid
+        
+        
+        //Mostrar el número de aeronaves en este momento en el aeropuerto de Granada
+        
+        
+        
+        
+        //Mostrar toda la información del Avion1
+        
+        //Mostrar toda la información del Avion2
+        
+        //Mostrar toda la información del Avion3
+        
+        
+        //----------------------------------------------
+        //          Llamadas a métodos estáticos
+        //----------------------------------------------
+        
+        
+        
+        
+        
+        //Mostrar el número total de aeronaves volando ahora mismo
+        
+        
+        //Mostrar el tiempo total de aeronaves volando en horas
+        
+        
+        //Mostrar el número total de aeronaves
+        
+        
+        
+        
+    }
+}
